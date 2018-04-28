3940247913819c49ab70c54a71978eed264a2114
Philip Japikse <skimedic@gmail.com>
2018-04-28 17:05:29 -0400

    added javascript client

--- DOTNETCORE/Security/IdentityServer/ApiResource/Startup.cs
+++ DOTNETCORE/Security/IdentityServer/ApiResource/Startup.cs
@@ -35,11 +35,23 @@ namespace ApiClient
 
                     options.ApiName = "myApi";
                 });
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5003")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("default");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- DOTNETCORE/Security/IdentityServer/IdentityServerWithIdentity/Config.cs
+++ DOTNETCORE/Security/IdentityServer/IdentityServerWithIdentity/Config.cs
@@ -101,6 +101,25 @@ namespace IdentityServerWithIdentity
                         "myApi"
                     },
                     AllowOfflineAccess = true
+                },
+                // JavaScript Client
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris =           { "http://localhost:5003/callback.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:5003" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "myApi"
+                    }
                 }
             };
         }
