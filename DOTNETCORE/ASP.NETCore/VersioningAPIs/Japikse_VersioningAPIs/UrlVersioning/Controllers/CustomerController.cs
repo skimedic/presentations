4d79d49f118cee826f938a979137f715758e6af9
Philip Japikse <skimedic@gmail.com>
2019-04-15 21:50:55 -0400

    removed space from directory name

---
+++ DOTNETCORE/ASP.NETCore/VersioningAPIs/Japikse_VersioningAPIs/UrlVersioning/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlVersioning.Controllers
+{
+
+    [ApiVersion("4.0")]
+    //[ApiVersion("1.1", Deprecated = true)]
+    [Route("api/v{version:apiVersion}/Customer")]
+    public class CustomerController : Controller
+    {
+        /// <summary>
+        /// Old version
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1v2", "value2v2" };
+        }
+        ///// <summary>
+        ///// New version
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpGet, MapToApiVersion("3.0")]
+        //public ActionResult<IEnumerable<string>> GetV3()
+        //{
+        //    return new string[] { "value1v2", "value2v2" };
+        //}
+
+    }
+}
---
+++ DOTNETCORE/ASP.NETCore/VersioningAPIs/Japikse_VersioningAPIs/UrlVersioning/Controllers/Values2Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlVersioning.Controllers
+{
+
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/Values")]
+    [ApiController]
+    public class Values2Controller : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1v2", "value2v2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "valuev2";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
