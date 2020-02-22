9ecff798ec778f33aae71fa5e63b982645380dcd
skimedic <skimedic@gmail.com>
2020-02-22 14:10:04 -0500

    final tweaks

--- DOTNETCORE/ASP.NETCore/ASP.NETCoreAndEfCore/SpyStore.Hol.Mvc/Controllers/HomeController.cs
+++ DOTNETCORE/ASP.NETCore/ASP.NETCoreAndEfCore/SpyStore.Hol.Mvc/Controllers/HomeController.cs
@@ -4,12 +4,17 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpyStore.Hol.Models.Entities;
+using SpyStore.Hol.Models.Entities.Base;
 using SpyStore.Hol.Mvc.Models;
 
 namespace SpyStore.Hol.Mvc.Controllers
 {
+    [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
+        [Route("/Home")]
+        [Route("/Home/Index")]
         public IActionResult Index()
         {
             return View();
@@ -24,6 +29,22 @@ namespace SpyStore.Hol.Mvc.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult RazorSyntax()
+        {
+            var prod = new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Details = new ProductDetails
+                {
+                    Description = "A Product",
+                    ModelName = "Model Name",
+                    ModelNumber = "Model Number"
+                }
+            };
+            return View(prod);
         }
     }
 }
