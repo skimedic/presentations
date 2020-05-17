6fc7edbb190a7dfaf16cfb9802396ce96cd02666
skimedic <skimedic@gmail.com>
2020-05-17 16:12:40 -0400

    add required fields

---
+++ DOTNETCORE/ASP.NETCore/AutoLot/AutoLot.Dal/EfStructures/Migrations/20200517200926_AddRequired.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoLot.Dal.EfStructures.Migrations
+{
+    public partial class AddRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PetName",
+                schema: "Dbo",
+                table: "Inventory",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                schema: "Dbo",
+                table: "Inventory",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PetName",
+                schema: "Dbo",
+                table: "Inventory",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                schema: "Dbo",
+                table: "Inventory",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
