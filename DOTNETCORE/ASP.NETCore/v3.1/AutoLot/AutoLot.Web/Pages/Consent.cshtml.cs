87117614990ed4e35391cae875777616705f22df
skimedic <skimedic@gmail.com>
2020-10-18 22:32:12 -0400

    added cookie policy

---
+++ DOTNETCORE/ASP.NETCore/v3.1/AutoLot/AutoLot.Web/Pages/Consent.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoLot.Web.Pages
+{
+    public class ConsentModel : PageModel
+    {
+        public IActionResult OnGet(string actionName)
+        {
+            if (actionName.Equals("grant", StringComparison.OrdinalIgnoreCase))
+            {
+                HttpContext.Features.Get<ITrackingConsentFeature>().GrantConsent();
+            }
+            else
+            {
+                HttpContext.Features.Get<ITrackingConsentFeature>().WithdrawConsent();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
