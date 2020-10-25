685957e6d75f0dc5b124182ec458eb3505fabfd1
skimedic <skimedic@gmail.com>
2020-10-25 15:07:06 -0400

    added mvc site

--- DOTNETCORE/ASP.NETCore/v3.1/AutoLot/AutoLot.Dal/EfStructures/MigrationHelpers.cs
+++ DOTNETCORE/ASP.NETCore/v3.1/AutoLot/AutoLot.Dal/EfStructures/MigrationHelpers.cs
@@ -22,18 +22,19 @@ namespace AutoLot.Dal.EfStructures
         public static void CreateCustomerOrderView(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql($@"
+                exec (N' 
                 CREATE VIEW [dbo].[CustomerOrderView]
                 AS
                     SELECT dbo.Customers.FirstName, dbo.Customers.LastName, dbo.Inventory.Color, dbo.Inventory.PetName, dbo.Makes.Name AS Make
                     FROM   dbo.Orders 
                     INNER JOIN dbo.Customers ON dbo.Orders.CustomerId = dbo.Customers.Id 
                     INNER JOIN dbo.Inventory ON dbo.Orders.CarId = dbo.Inventory.Id
-                    INNER JOIN dbo.Makes ON dbo.Makes.Id = dbo.Inventory.MakeId 
-                ");
+                    INNER JOIN dbo.Makes ON dbo.Makes.Id = dbo.Inventory.MakeId
+                ')");
         }
         public static void DropCustomerOrderView(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("DROP VIEW [dbo].[CustomerOrderView]");
+            migrationBuilder.Sql("EXEC (N' DROP VIEW [dbo].[CustomerOrderView] ')");
         }
     }
 }
---
+++ DOTNETCORE/ASP.NETCore/v5.0/AutoLot50/AutoLot.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using AutoLot.Dal.Repos.Interfaces;
+using AutoLot.Models.ViewModels;
+using AutoLot.Mvc.Extensions;
+using AutoLot.Mvc.Models;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AutoLot.Mvc.Controllers
+{
+    [Route("[controller]/[action]")]
+    //[Route("Home/[action]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [Route("/")]
+        [Route("/[controller]")]
+        [Route("/[controller]/[action]")]
+        [HttpGet]
+        public IActionResult Index([FromServices] IOptionsMonitor<DealerInfo> dealerMonitor)
+        {
+            var vm = dealerMonitor.CurrentValue;
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RazorSyntax([FromServices] ICarRepo carRepo)
+        {
+            var car = carRepo.Find(1);
+            return View(car);
+        }
+
+        [HttpGet]
+        public IActionResult GrantConsent()
+        {
+            HttpContext.Features.Get<ITrackingConsentFeature>().GrantConsent();
+            return RedirectToAction(nameof(Index), nameof(HomeController).RemoveController(),
+                new {area = ""});
+        }
+
+        [HttpGet]
+        public IActionResult WithdrawConsent()
+        {
+            HttpContext.Features.Get<ITrackingConsentFeature>().WithdrawConsent();
+            return RedirectToAction(nameof(Index), nameof(HomeController).RemoveController(),
+                new {area = ""});
+        }
+    }
+}
