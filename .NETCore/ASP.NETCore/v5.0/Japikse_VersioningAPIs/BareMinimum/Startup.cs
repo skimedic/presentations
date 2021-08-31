bd441d4bddc51d1711da15dc409a4c417a7760f9
Philip Japikse <skimedic@gmail.com>
2021-08-31 12:27:10 -0400

    added versioning for ASP.NET Core 5

---
+++ .NETCore/ASP.NETCore/v5.0/Japikse_VersioningAPIs/BareMinimum/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BareMinimum
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddApiVersioning(options=>
+            {
+                // reporting api versions will return the headers "api-supported-versions"
+                // and "api-deprecated-versions"
+                options.ReportApiVersions=true;
+                //Only version [ApiController] defaults to true in 3.1+
+                options.UseApiBehavior = true;
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BareMinimum", Version = "v1" });
+                //c.ResolveConflictingActions(c=>c.First());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BareMinimum v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ .NETCore/ASP.NETCore/v5.0/Japikse_VersioningAPIs/FullSwaggerSupport/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullSwaggerSupport.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("1.5", Deprecated = true)]
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        //[MapToApiVersion("2.0")]
+        [ApiVersion("1.0")]
+        [ApiVersion("2.0")]
+        public string Get(ApiVersion apiVersion)
+            => $"Controller = {GetType().Name}\nVersion = {apiVersion}";
+
+        [HttpGet("{id}")]
+        public string Get2(int id)
+        {
+            ApiVersion version = HttpContext.GetRequestedApiVersion();
+            return $"Controller = {GetType().Name}\nVersion = {version}";
+        }
+
+        [HttpGet]
+        [ApiVersion("1.5")]
+        public string Get2(ApiVersion apiVersion)
+            => $"Controller = {GetType().Name}\nVersion = {apiVersion}";
+
+        [HttpGet]
+        [MapToApiVersion("3.0")]
+        public string Get3(ApiVersion apiVersion)
+            => $"Controller = {GetType().Name}\nVersion = {apiVersion}";
+
+    }
+}
