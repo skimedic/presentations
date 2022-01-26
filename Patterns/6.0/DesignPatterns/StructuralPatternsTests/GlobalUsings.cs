49c5229e9cd8b117d226fac6b2ddb8d4e3711eef
Philip Japikse <skimedic@gmail.com>
2022-01-25 19:55:20 -0500

    added decorator tests

--- Patterns/6.0/DesignPatterns/StructuralPatternsTests/GlobalUsings.cs
+++ Patterns/6.0/DesignPatterns/StructuralPatternsTests/GlobalUsings.cs
@@ -11,4 +11,8 @@ global using StructuralPatterns.Adapter.Interfaces;
 global using StructuralPatterns.Facade.CleanerCode;
 global using StructuralPatterns.Facade.CleanerCode.Interfaces;
 
+global using StructuralPatterns.Decorator.CarExample;
+global using StructuralPatterns.Decorator.CarExample.Base;
+global using StructuralPatterns.Decorator.CarExample.Interfaces;
+
 global using Xunit;
